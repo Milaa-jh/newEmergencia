ed6119bf070b0c0f7d5e9dc74d1197d47faac04c
Eltomass <165742034+Eltomass@users.noreply.github.com>
2024-11-14 21:15:34 -0300

    login solucionado

--- src/app/page/tab-inicial/tab-inicial.page.ts
+++ src/app/page/tab-inicial/tab-inicial.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router'; // Importar Router
 
 @Component({
   selector: 'app-tab-inicial',
@@ -13,12 +14,15 @@ export class TabInicialPage {
     password: ''
   };
 
-  constructor() {}
+  constructor(private router: Router) {} // Inyectar Router en el constructor
 
   onLogin() {
     // implementar la conexión con la BDD
     console.log('Datos de login:', this.loginData);
     //simula un login exitoso
     this.isLoggedIn = true;
+    
+    // Después de un login exitoso, redirigir al usuario
+    this.router.navigate(['/tab-inicial/boton-sos']); // Asumiendo que el botón de emergencia está en la página home
   }
 }
